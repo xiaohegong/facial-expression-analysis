0144ba31f800967ee0f64f43facff91ba8ace99a
Jiatao Xiang <a389157511@gmail.com>
2020-12-16 22:54:39 -0500

    add predict method

--- model/vgg16.py
+++ model/vgg16.py
@@ -2,6 +2,7 @@ import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import cv2 as cv
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,33 +22,57 @@ class VGG16(nn.Module):
 
         self.block1 = nn.Sequential(
             nn.Conv2d(1, 64, 3),  # input channel, output channel, kernel size
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.Conv2d(64, 64, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
 
         self.block2 = nn.Sequential(
             nn.Conv2d(64, 128, 3), 
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
             nn.Conv2d(128, 128, 3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
 
         self.block3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3), 
+            nn.Conv2d(128, 256, 3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.Conv2d(256, 256, 3),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
 
         self.block4 = nn.Sequential(
-            nn.Conv2d(256, 512, 3), 
+            nn.Conv2d(256, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             nn.Conv2d(512, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             nn.Conv2d(512, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
 
         self.block5 = nn.Sequential(
-            nn.Conv2d(512, 512, 3), 
             nn.Conv2d(512, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             nn.Conv2d(512, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
             nn.MaxPool2d(2, stride=2)
         )
         
@@ -55,10 +80,11 @@ class VGG16(nn.Module):
 
         self.fc1 = nn.Linear(input_dims, 4096)
         self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, 1000)
-        self.fc4 = nn.Linear(1000, self.num_classes)
+        self.fc3 = nn.Linear(4096, 7)
+        # self.fc4 = nn.Linear(1000, self.num_classes)
         
-        self.optimizer = optim.Adam(self.parameters(), lr=self.alpha)
+        # self.optimizer = optim.Adam(self.parameters(), lr=self.alpha) 
+        self.optimizer = optim.SGD(self.parameters(), lr=self.alpha, momentum=0.9, weight_decay=5e-4)
 
         self.loss = nn.CrossEntropyLoss()
         self.to(self.device)
@@ -76,26 +102,29 @@ class VGG16(nn.Module):
     def forward(self, batch_data):
 
         batch_data = self.block1(batch_data)
-        batch_data = F.relu(batch_data)
+        # batch_data = F.relu(batch_data)
 
         batch_data = self.block2(batch_data)
-        batch_data = F.relu(batch_data)
+        # batch_data = F.relu(batch_data)
 
         batch_data = self.block3(batch_data)
-        batch_data = F.relu(batch_data)
+        # batch_data = F.relu(batch_data)
 
         batch_data = self.block4(batch_data)
-        batch_data = F.relu(batch_data)
+        # batch_data = F.relu(batch_data)
 
         batch_data = self.block5(batch_data)
-        batch_data = F.relu(batch_data)
+        # batch_data = F.relu(batch_data)
 
         batch_data = batch_data.view(batch_data.size()[0], -1)
 
         batch_data = self.fc1(batch_data)
+        batch_data = F.relu(batch_data)
         batch_data = self.fc2(batch_data)
-        batch_data = self.fc3(batch_data)
-        classes = self.fc4(batch_data)
+        batch_data = F.relu(batch_data)
+        classes = self.fc3(batch_data)
+        # batch_data = F.relu(batch_data)
+        # classes = self.fc4(batch_data)
         return classes
 
     def get_data(self):
@@ -146,7 +175,7 @@ class VGG16(nn.Module):
         ep_acc = []
         for _, (input, label) in enumerate(self.test_data_loader):
             input = T.tensor(input)
-            input = T.reshape(input, (1, 1, 48, 48)).to(self.device)
+            input = T.reshape(input, (1, 1, 224, 224)).to(self.device)
             label = T.tensor([label])
             label = label.type(T.LongTensor).to(self.device)
             prediction = self.forward(input)
@@ -164,3 +193,12 @@ class VGG16(nn.Module):
 
         print('total loss %.3f' % ep_loss,
                 'accuracy %.3f' % np.mean(ep_acc))
+    
+    def predict(self, image):
+        # print(image.shape)
+        image = cv.resize(image, (224, 224), interpolation=cv.INTER_AREA)
+        image = T.reshape(T.tensor(image), (1, 1, 224, 224)).float().to(self.device)
+        prediction = self.forward(image)
+        prediction = F.softmax(prediction, dim=1)
+        class_ = T.argmax(prediction, dim=1)
+        return prediction, class_
