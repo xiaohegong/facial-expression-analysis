2ef50ca8615cee59063500c088f8568b13d1e783
Bohan <bhjiang@outlook.com>
2020-12-18 18:55:45 +0800

    add hog feature

--- save_bipart_data.py
+++ save_bipart_data.py
@@ -30,9 +30,11 @@ if __name__ == "__main__":
     #samples = []
     eyes_samples = []
     mouth_samples = []
+    hogs_eyes = []
+    hogs_mouth = []
     labels = []
 
-    datapath = os.path.join('data','CK_bipart_data.h5')
+    datapath = os.path.join('data','CK_bipart_hog.h5')
 
     if not os.path.exists(os.path.dirname(datapath)):
         os.makedirs(os.path.dirname(datapath))
@@ -45,17 +47,20 @@ if __name__ == "__main__":
         for image in images:
             image = cv.imread(os.path.join(folder,image))
             # samples.append(image)
-            eyes, mouth = detect_bipart(image)
+            mouth, eyes, hog_eyes, hog_mouth = detect_bipart(image)
             eyes_samples.append(eyes)
             mouth_samples.append(mouth)
+            hogs_eyes.append(hog_eyes)
+            hogs_mouth.append(hog_mouth)
             labels.append(i)
 
-    print(np.shape(eyes_samples))
-    print(np.shape(labels))
-
+    print(np.shape(hogs_eyes))
+    print(np.shape(hogs_mouth))
 
     datafile = h5py.File(datapath, 'w')
     datafile.create_dataset("eyes_samples", dtype = 'float32', data=eyes_samples)
     datafile.create_dataset("mouth_samples", dtype = 'float32', data=mouth_samples)
+    datafile.create_dataset("hog_eyes", dtype = 'float32', data=hogs_eyes)
+    datafile.create_dataset("hog_mouth", dtype = 'float32', data=hogs_mouth)
     datafile.create_dataset("data_labels", dtype = 'int32', data=labels)
     datafile.close()
